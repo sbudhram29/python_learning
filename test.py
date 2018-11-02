3d5534f26f2b659e3fb04249723a3d82d95e435c
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-11-02 10:55:56 -0400

    add coverage

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ class FibTests(unittest.TestCase):
     def setUp(self):
         self.fib10 = fib(10)
         self.fib1 = fib(1)
+        self.fib0 = fib(0)
 
     def test_five_plus_five(self):
         assert 5 + 5 == 10
@@ -20,9 +21,13 @@ class FibTests(unittest.TestCase):
     def test_fib_10_is_89(self):
         assert self.fib10 == 55
 
+    def test_fib_0_is_0(self):
+        assert self.fib0 == 0
+
 
 """
 run auto
+instead of python3 -m unittest test.py
 """
 
 if __name__ == "__main__":
