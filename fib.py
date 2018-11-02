c1fddf17b04d56b5395ebeb1c496143ed2f78903
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-11-02 10:10:05 -0400

    add doctests

--- fib.py
+++ fib.py
@@ -1,15 +1,23 @@
+"""
+Fib
+"""
 from functools import lru_cache
+
 
 @lru_cache()
 def fib(n):
-    if n == 0:
-        return 1
-    if n == 1:
+    """
+    >>> fib(38)
+    39088169
+    """
+    if n <= 0:
+        return 0
+    if n <= 2:
         return 1
     return fib(n-1) + fib(n-2)
 
 
 number = int(input("please end number: "))
 
-for n in range(1, number):
+for n in range(number):
     print(f"{n}: {fib(n)}")
