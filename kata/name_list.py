5f73465f62dd74372f039d5131cb8b06c3993c9d
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-11-19 11:14:38 -0500

    add name_list

---
+++ kata/name_list.py
@@ -0,0 +1,29 @@
+def namelist(names):
+    if not(len(names)):
+        return ''
+    if len(names) == 1:
+        return names[0]['name']
+
+    # loop throught names and add to array
+
+    arr = []
+    for name in names:
+        arr.append(name['name'])
+    # create string with separated by comma
+    last = arr.pop()
+
+    begin = ', '.join(arr)
+
+    return begin + ' & ' + last
+
+    # add a & for the last name
+
+
+print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]))
+# returns 'Bart, Lisa & Maggie'
+
+print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}]))
+# returns 'Bart & Lisa'
+
+print(namelist([{'name': 'Bart'}]))
+# returns 'Bart'
