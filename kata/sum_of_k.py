fa0aabe7c0247c63cc4a412a10d8edeb2c3a6fc3
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-11-26 13:28:37 -0500

    new Kata

---
+++ kata/sum_of_k.py
@@ -0,0 +1,17 @@
+def sum_of_k(list_of_numbers, k):
+
+    seen = []
+    for n in list_of_numbers:
+        needed = k - n
+        if needed in seen:
+            return True
+        else:
+            seen.append(n)
+
+    return False
+
+
+nums = [10, 15, 3, 7]
+k = 17
+
+print(sum_of_k(nums, k))
