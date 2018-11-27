0ef9bdec00bb5673249c1a120c94f74aabace228
Sean Budhram <sbudhram@carlisle-etcetera.com>
2018-11-27 16:21:47 -0500

    started on node serialize

---
+++ kata/node_serialize.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+
+
+def serialize(node):
