34c99893b99defad74d51699e26081d1b37b1a41
sbudhram29 <sbudhram@gmail.com>
2019-02-06 19:20:46 -0500

    completed kata and added test

--- kata/string_matches.py
+++ kata/string_matches.py
@@ -1,19 +1,28 @@
 
-def find_longest(s1, s2):
-
-    matches = []
+def find_longest(s1, s2, matches = []):
+    current_matches = []
     s2Arr = list(s2)
 
     for letter in s1:
         for k, v in enumerate(s2Arr):
             if letter == v:
-                matches.append(letter)
+                current_matches.append(letter)
                 if k == 0:
                     del s2Arr[k]
                 else:
-                    s2Arr = s2Arr[k:]
+                    s2Arr = s2Arr[k+1:]
                 break
-    return ''.join(matches)
+
+    if len(current_matches) > len(matches):
+        matches = [*current_matches]
+
+    if len(matches) < len(s1):
+        return find_longest(s1[1:], s2, matches)
+    else:
+        return ''.join(matches)
+
+
+
 
 
 # print(find_longest('ABAXDC', 'BACBAD'))
--- kata/string_matches_test.py
+++ kata/string_matches_test.py
@@ -5,9 +5,12 @@ import string_matches as match
 class string_matches_test(unittest.TestCase):
 
     def test_longest_match(self):
-        self.assertEquals(match.find_longest('ABAXDC', 'BACBAD'), 'ABAD')
-        self.assertEquals(match.find_longest('aaaa', 'aa'), 'aa')
-        self.assertEquals(match.find_longest('AGGTAB', 'GCTXAYB'), 'GTAB')
+        self.assertEqual(match.find_longest('ABAXDC', 'BACBAD'), 'ABAD')
+        self.assertEqual(match.find_longest('aaaa', 'aa'), 'aa')
+        self.assertEqual(match.find_longest('AGGTAB', 'GCTXAYB'), 'GTAB')
+        self.assertEqual(match.find_longest('seanbudhram', 'sxecadn'), 'sean')
+        self.assertEqual(match.find_longest('seanbudhram', 'seanbudhram'), 'seanbudhram')
+        self.assertEqual(match.find_longest('sbudhram', 'sbudhram'), 'sbudhram')
 
 
 if __name__ == '__main__':
