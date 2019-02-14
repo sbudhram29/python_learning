9725c819a0c1f1a62270ef9c7d4a6449278ffa11
Sean Budhram <sbudhram@carlisle-etcetera.com>
2019-02-14 09:21:28 -0500

    list comprehension testing

--- advance/listComprehension.py
+++ advance/listComprehension.py
@@ -3,10 +3,18 @@ nums = range(5, 201)
 halves = []
 
 for num in nums:
-    halves.append(num/2)
+    halves.append(num / 2)
 
 print(halves)
 
 halves = [num/2 for num in nums]
 
 print(halves)
+
+fizzBuzz = (lambda x: 'Fizz' * (x % 3 == 0) + 'Buzz' * (x % 5 == 0) or str(x))
+
+print([fizzBuzz(x) for x in range(1, 101)])
+
+print(1 * (2 % 2 == 1) or False)
+print(1 * (2 % 2 == 0) or False)
+
