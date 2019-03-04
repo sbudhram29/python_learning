c59bfd93285366deda38b901c669080c0cc865a1
sbudhram29 <sbudhram@gmail.com>
2019-03-03 22:29:47 -0500

    serialize node

--- kata/node_serialize.py
+++ kata/node_serialize.py
@@ -3,9 +3,43 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
+    def __str__(self):
+        return f"Node('{self.val}', {self.left}, {self.right})"
+
+class Linked:
+    def __init__(self, head):
+        if type(head) != Node:
+            self.head = Node(head)
+        else:
+            self.head = head
+
+    def __str__(self):
+        current = self.head
+
+        result = ""
+
+        while current.left or current.right:
+            result += f"Node('{current.val}', "
+            if current.left:
+                result += f"left = Node({current.left}), "
+            else:
+                result += f"left = None, "
+
+            if current.right:
+                result += f"right = Node({current.right}))"
+            else:
+                result += f"right = None)"
+            current = current.left
+
+        return result
 
 
-node = Node('root', Node('left', Node('left.left')), Node('right'))
 
 
-def serialize(node):
+
+
+
+node = Linked(Node('root',Node('left', Node('left.left')),Node('right')))
+
+print(node)
+
