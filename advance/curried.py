a1b7cbf4e12d39b2dbd98fe421b5b5e838aa0f29
Sean Budhram <sbudhram@carlisle-etcetera.com>
2019-03-11 09:36:47 -0400

    worked on collection and curried

--- advance/curried.py
+++ advance/curried.py
@@ -2,10 +2,12 @@ def curried_f(x, y=None, z=None):
     def f(x, y, z):
         return x**3 + y**2 + z
 
-    if(y is not None and z is not None):
+    if y is not None and z is not None:
         return f(x, y, z)
-    if(y is not None):
+
+    if y is not None:
         return lambda z: f(x, y, z)
+
     return lambda y, z=None: (
         f(x, y, z) if (y is not None and z is not None)
         else (lambda z: f(x, y, z))
